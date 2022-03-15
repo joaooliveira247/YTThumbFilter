27c1d9c8ee6d4eca08b20a57da2cbc336b0c98e0
joaooliveira247 <joaooliveira247@gmail.com>
2022-03-15 15:58:39 -0300

    vai na fé

--- YTTF.py
+++ YTTF.py
@@ -2,10 +2,29 @@ from resources.yt_Resources import YTThumb
 import easyocr
 
 
-def test(url: str, gpu: str = False):
-    _info = YTThumb(url).thumb_info()
-    _ocr = easyocr.Reader(['pt', 'en'], gpu=gpu)
-    result = _ocr.readtext(_info['img_url'])
-    for _bbox, text, prob in result:
-        if prob > 0.5:
-            print(text)
+class YTStats():
+
+    def single_video(url: str, gpu: str = False, download: str = False):
+        try:
+            _info = YTThumb(url).thumb_info(download=download)
+            _ocr = easyocr.Reader(['pt', 'en'], gpu=gpu)
+            result = _ocr.readtext(_info['img_url'])
+            words = []
+            for _bbox, text, prob in result:
+                if prob > 0.5:
+                    words.append(text)
+
+            return {
+                'text': ' '.join(words), 'title': _info['title'],
+                'url': _info['url']
+                }
+
+        except Exception as error:
+            raise error
+
+    # def channel_videos(url: str, gpu: str = False):
+    #     try:
+    #         _info = YTThumb(url)
+
+
+print(YTStats.single_video("https://www.youtube.com/watch?v=Xl5T6zFUezc"))
--- resources/yt_Resources.py
+++ resources/yt_Resources.py
@@ -1,6 +1,8 @@
 from urllib import request
 from pytube import YouTube
+from pytube.contrib.channel import Channel
 from pathlib import Path
+import re
 
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -8,19 +10,39 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 class YTThumb():
     def __init__(self, url: str):
+        # if re.match(
+        #     (r'(?:https:\/\/)?(:?www\.)?(?:youtu)(?:[.be]{3})?(?:com)?'
+        #      r'\/watch\?v=[\w\-\_]{11}'), url):
         self.url = url
-        self.dir = dir
+        # else:
+        #     raise Exception('Invalid URL.')
 
-    def thumb_info(self) -> None:
-        self.title = YouTube(self.url).title
-        self.img_url = YouTube(self.url).thumbnail_url
+    def thumb_info(self, dir: str = None, download: bool = False):
+        title = YouTube(self.url).title
+        img_url = YouTube(self.url).thumbnail_url
+        views = YouTube(self.url).views
+        if download:
+            request.urlretrieve(
+                img_url, BASE_DIR / (self.title + '.jpg'))
+            if dir:
+                request.urlretrieve(
+                    img_url, BASE_DIR / self.dir / (self.title + '.jpg'))
         return {
-            "title": self.title, "img_url": self.img_url, "url": self.url
+            "title": title, "img_url": img_url, "url": self.url, "views": views
             }
 
-    def thumb_download(self, dir: str = None):
-        request.urlretrieve(
-            self.img_url, BASE_DIR / (self.title + '.jpg'))
-        if self.dir:
-            request.urlretrieve(
-                self.img_url, BASE_DIR / self.dir / (self.title + '.jpg'))
+    def thumb_info_channel(self, dir: str = None, download: bool = False):
+        max = len(Channel(self.url))
+        list = []
+        for video in Channel(self.url):
+            title = YouTube(video).title
+            img_url = YouTube(video).thumbnail_url
+            views = YouTube(video).views
+            list.append(
+                {
+                    "title": title, 'img_url': img_url, 'url': self.url,
+                    'views': views
+                    }
+                )
+        return list
+
